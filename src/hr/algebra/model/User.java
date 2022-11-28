14965750d42ea58aec3beaff0a24c1ba6b37579b
ozadro <79858635+ozadro@users.noreply.github.com>
2022-11-28 13:14:49 +0100

    first commit

---
+++ src/hr/algebra/model/User.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hr.algebra.model;
+
+/**
+ *
+ * @author Korisnik
+ */
+public class User {
+    private int id;
+    public String username;
+    public String password;
+    public boolean admin;
+
+  
+
+    public User() {
+    }
+
+    public User(int id, String username, String password, boolean admin) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.admin = admin;
+    }
+
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+      public boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
+    
+    
+}
